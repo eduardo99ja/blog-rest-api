889be59dad9dac26b94dc4cf62c0d0d9e70219aa
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-12-25 22:43:47 -0600

    Ordering in sorting

--- src/main/java/com/apodaca/blog/service/PostService.java
+++ src/main/java/com/apodaca/blog/service/PostService.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface PostService {
     PostDto createPost(PostDto postDto);
 
-    PostResponse getALlPosts(int pageNo, int pageSize, String sortBy);
+    PostResponse getALlPosts(int pageNo, int pageSize, String sortBy,String sortDir);
 
     PostDto getPostById(long id);
 
