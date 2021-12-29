581448ef59496d7d286a4ec1df32bd6e6461b28f
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-12-28 22:37:41 -0600

    Get all comments by post id api

--- src/main/java/com/apodaca/blog/service/CommentService.java
+++ src/main/java/com/apodaca/blog/service/CommentService.java
@@ -2,8 +2,11 @@ package com.apodaca.blog.service;
 
 import com.apodaca.blog.payload.CommentDto;
 
+import java.util.List;
+
 public interface CommentService {
     CommentDto createComment(long postId, CommentDto commentDto);
+    List<CommentDto> getCommentsByPostId(long postId);
 
 
 }
--- src/main/java/com/apodaca/blog/service/impl/CommentServiceImpl.java
+++ src/main/java/com/apodaca/blog/service/impl/CommentServiceImpl.java
@@ -10,6 +10,9 @@ import com.apodaca.blog.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CommentServiceImpl implements CommentService {
 
@@ -38,6 +41,16 @@ public class CommentServiceImpl implements CommentService {
         Comment newComment = commentRepository.save(comment);
 
         return mapToDTO(newComment);
+    }
+
+    @Override
+    public List<CommentDto> getCommentsByPostId(long postId) {
+        // retrieve comments by postId
+        List<Comment> comments = commentRepository.findByPostId(postId);
+
+        // convert list of comment entities to list of comment dto's
+//        return comments.stream().map(comment -> mapToDTO(comment)).collect(Collectors.toList());
+        return comments.stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
     private CommentDto mapToDTO(Comment comment) {
